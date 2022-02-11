599e7d6748635269991693fdc6530004fe9a7e7a
IsaacH <Isaac.hathaway28@gmail.com>
2022-02-11 08:46:52 -0700

    create html template, link js and css, and add a css starter"

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Create an html template and copy over css starter from course supplement page.
+// Create html elements: h1, h2, 42divs, and reset button.
+// Create a grid in css for chips to be shown in. Similar to tic tac toe, but expanded to a 6row and 7 column grid.
+    // grid-column: repeat (7, )
+// State variables in JS.
+    //
+// Add cached elements References.
+    //div squares 
+        //this will be used 
+    //h2 message 
+        // init () message will say click a square to put first chip
+        // message will then tell whose players turn it is 
+// Add event listeners for button clicks for replay button and  handleClick events.
+    // Replay -- on click return game state to  init
+    // handleClick -- target evt.target.id (div squares)
+// console.log everything so far to make sure it's working.
+// Add initial state function and add necessary variables. 
+    // board array {null,}
+    // winner = null
+    // reset button = hidden
+    //render()
+    //player turn = -1 (player 1)
+
+// Add render function .
+// State of game :
+// Winner, tie(unlikely), and whose turn
+// Add function store clicks in an array.
+// Define winning combinations.
+// Possibly use another js file and import this info.
+// Add a winner function to determine if someone has won.
+// If there is a winner,  style with confetti.
+// Confetti colors to mimic winners colors.
+// Add sound effects to column clicks and winner clicks. 
+// Add a favicon of something cool.
+// Add dark mode with optional neon lit chips, buttons, and game board.
+// Optional - implement a function that will allow a powerup ability.
+// 1x powerup chance per person. 
+// Swap colors of a single chip if successful.
+// Success will be determined with a Math.floor(Math.random().
+// Optional -  implement an animation that will show the chips fall from the top once the players click the column they want.
