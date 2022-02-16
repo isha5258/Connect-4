2fe2a0dc9df2bd007c8824b7eca861ab2f6c5d08
IsaacH <Isaac.hathaway28@gmail.com>
2022-02-16 14:48:12 -0700

    adjust text shadow for tie message

--- js/app.js
+++ js/app.js
@@ -166,7 +166,7 @@ function render() {
   } else if (winner === "T") {
     messageEl.textContent = "Tie! Let's try again!"
     messageEl.style.color = '#667D83'
-    messageEl.style.textShadow = `${playerTurn === 1 || playerTurn === -1 ?  '0 0 10px black, 0 0 15px black, 0 0 20px black, 0px 0px 30px black, 0 0 40px' : ''}`
+    messageEl.style.textShadow = `${playerTurn === 1 || playerTurn === -1 ?  '10px 0 10px black, 20px 0 20px black, 30pxpx 0px 30px black' : ''}`
   } else {
     messageEl.textContent = `Congratulations, ${winner === 1 ? 'Red' : 'Blue'} Wins!!!!`
     messageEl.style.color = `${winner === 1 ? '#CC2727' : '#0073BF'}`
